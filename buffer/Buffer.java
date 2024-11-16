9f9e6a6fdea77c63aa89698abe3b53d3a8d7d4ba
Dheraj07 <dherajmajeti2005@gmail.com>
2024-11-16 16:15:40 +0530

    nndd

--- buffer/Buffer.java
+++ buffer/Buffer.java
@@ -1,37 +1,52 @@
 package buffer;
+//using locks concept
 
 import java.util.LinkedList;
+import java.util.concurrent.locks.Condition;
+import java.util.concurrent.locks.Lock;
+import java.util.concurrent.locks.ReentrantLock;
 
 public class Buffer {
    private LinkedList<Integer> b;
    private static final int MAXSIZE=3;
 
+   private static Lock l=new ReentrantLock();
+   private static Condition pc=l.newCondition();
+   private static Condition cc=l.newCondition();
+
    public Buffer(){
        b=new LinkedList<>();
    }
 
-   public synchronized void produce(int e) throws InterruptedException {
+   public void produce(int e) throws InterruptedException {
+       l.lock();
         while (b.size()>=MAXSIZE){
             System.out.println("Producer is waiting because buffer is full");
-            wait();
+            pc.await();
         }
         b.add(e);
-        notify();//It will break wait of other thread
+
        System.out.println("Produced: "+e);
+       cc.signal();
 
    }
 
    public synchronized void consume()  {
+       l.lock();
        while (b.isEmpty()){
            System.out.println("Consumer is waiting because Buffer is empty");
            try {
-               wait();
+               cc.await();
            } catch (InterruptedException e) {
                throw new RuntimeException(e);
            }
-           notify();
+
        }
 
        System.out.println("Consumed "+b.removeFirst());
+       pc.signal();
    }
 }
+
+
+//semafore
