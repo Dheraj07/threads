4c83a03477736cba146caa1f37c31177f33fab94
Dheraj07 <dherajmajeti2005@gmail.com>
2024-11-16 15:45:56 +0530

    nndd

---
+++ buffersyn/SyncMain.java
@@ -0,0 +1,40 @@
+package buffersyn;
+
+
+public class SyncMain {
+    public static void main(String[] args) {
+        Buffer b= new Buffer();
+
+        Thread tp=new Thread(
+                ()->{
+                    try {
+                        int e=0;
+                        while (true) {
+                            b.produce(e++);
+                            Thread.sleep(2000);
+                        }
+                    }
+                    catch (InterruptedException ex) {
+                        throw new RuntimeException(ex);
+                    }
+
+                }
+        );
+
+        Thread tc=new Thread(
+                ()->{
+                    while (true){
+                        b.consume();
+                        try {
+                            Thread.sleep(6000);
+                        } catch (InterruptedException e) {
+                            throw new RuntimeException(e);
+                        }
+                    }
+                }
+        );
+
+        tp.start();
+        tc.start();
+    }
+}
